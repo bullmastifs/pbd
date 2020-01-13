75d829bacde528cb2cc849c5275e22e538c401b6
Aleksandrs Belikovs Air <alekss@enter7.lv>
2020-01-14 00:27:05 +0200

    Global changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,25 @@
-//jquery smooth animation
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1200, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-});
+// Get the modal
+var modal = document.getElementById("myModal");
 
-//form actions
-$(document).ready(function(){
-  $("#hide").click(function(){
-    $("#form").hide(400);
-  });
-  $("#show").click(function(){
-    $("#form").show(400);
-  });
-  $("#mySidenav").on('click', function(event) {console.log("closing");    
-    closeNav();});
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-//scroll up button features
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  if (y < 800) {
-    $('.btn-up').fadeOut();
-  } else {
-    $('.btn-up').fadeIn();
-  }
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-//side menu
-function openNav() {
-  document.getElementById("mySidenav").style.width = "400px";
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
