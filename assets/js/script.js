9aa988b7763b5d9a47f19f2a31142016da35d5ce
Aleksandrs Belikovs Air <alekss@enter7.lv>
2020-01-15 00:17:07 +0200

    Justify aligns + procents on paddings

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,18 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+//jquery smooth animation
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
